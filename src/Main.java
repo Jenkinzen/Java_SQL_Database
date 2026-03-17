e3bcf11b01468410d6d29783d80796f233aad88d
Gordon <ggordonhuesch94@gmail.com>
2026-03-17 22:55:04 +0100

    new functions/started UI/Hashmapped every Column

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 import java.sql.Array;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-public class Main
-{
+public class Main {
 
 
-    public static void main(String[] args) throws SQLException
-    {   Connection.connection();
+    public static void main(String[] args) throws SQLException {
+        Connection.connection();
 
         ///CONNECTION/////////////////////////////////////////////////////////////////////////////////////
         var stmt = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\ggord\\IdeaProjects\\neues_projekt\\chinook.db").createStatement();
@@ -20,18 +16,14 @@ public class Main
         ///SQL DATASOURCE|PAYMENTINFOS////////////////////////////////////////////////////////////////////
         var paymentinfos = stmt.executeQuery("SELECT BillingAddress,CustomerId,Total FROM invoices");
 
-        while (paymentinfos.next())
-        {
+        while (paymentinfos.next()) {
             String customerid = paymentinfos.getString("CustomerId");
             Double bill = paymentinfos.getDouble("Total");
 
 
-
-
             //Erstellen einer HashMap und Rechnung einfügen insofern es für die customerid noch keine gibt
 
-            Service.getandroundfinalsum(customer_bill_by_id,customerid,bill);
-
+            Service.getandroundfinalsum(customer_bill_by_id, customerid, bill);
 
 
         }
@@ -40,9 +32,7 @@ public class Main
         var customerinfos = stmt.executeQuery("SELECT * FROM customers");
 
 
-
-        while (customerinfos.next())
-        {
+        while (customerinfos.next()) {
 
             String customerid = customerinfos.getString("CustomerId");
             String firstname = customerinfos.getString("FirstName");
@@ -61,65 +51,165 @@ public class Main
             Double bill = customer_bill_by_id.get(customerinfos.getString("CustomerId"));
 
 
+            Customers c = new Customers(customerid, firstname, lastname, address, city, country, company, state, phone, postalcode, fax, email, supportrepid, bill);
 
-            Customers c = new Customers(customerid,firstname,lastname,address,city,country,company,state,phone,postalcode,fax,email,supportrepid,bill);
 
-                    //Customer(Value) werden nach Anfangsbuchstabe des Nachnamens(Key) in customer_sorted_by_lastname gepackt
-                    Service.sortingbylastnamestartingletters(lastname,customers_sorted_by_lastname,c);
+            //Customer(Value) werden nach Anfangsbuchstabe des Nachnamens(Key) in customer_sorted_by_lastname_letter gepackt
+            Service.sortedbystartingletters(firstname, customers_sorted_by_firstname_letter, c);
+            Service.sortedbystartingletters(lastname, customers_sorted_by_lastname_letter, c);
 
-                    //Customer(Value) werden in Länder(Key) unterteilt
-                    Service.createkeyandfillvaluesStringArray(country,customers_sorted_by_country,c);
 
-                    //Customer(Value) wird seiner customerid zugeteilt
+            //Mehrere Values werden einem Key zugewiesen
+            Service.createkeyandfillvaluesMultiValue(address, customers_sorted_by_address, c);
+            Service.createkeyandfillvaluesMultiValue(city,customers_sorted_by_city,c);
+            Service.createkeyandfillvaluesMultiValue(country, customers_sorted_by_country, c);
+            Service.createkeyandfillvaluesMultiValue(state, customers_sorted_by_state, c);
+            Service.createkeyandfillvaluesMultiValue(company, customers_sorted_by_company,c);
+            Service.createkeyandfillvaluesMultiValue(postalcode,customers_sorted_by_postalcode,c);
+            Service.createkeyandfillvaluesMultiValue(supportrepid,customers_sorted_by_supportrepid,c);
 
-                    Service.createkeyandfillvaluesStringArrayDirectAdd(customerid,customers_sorted_by_id,c);
+            //Eine Value wird einem Key zugewiesen
+            Service.createkeyandfillvaluesSingleValue(lastname, customers_sorted_by_lastname, c);
+            Service.createkeyandfillvaluesSingleValue(firstname, customers_sorted_by_firstname, c);
+            Service.createkeyandfillvaluesSingleValue(customerid, customers_sorted_by_id, c);
+            Service.createkeyandfillvaluesSingleValue(email,customers_sorted_by_email,c);
 
-                    //Übergeordnete HashMap (quasi Hauptmenü um sachen bezüglich Customer zu finden)
 
-                    customersortedbycategories.put("last_name",customers_sorted_by_lastname);
-                    customersortedbycategories.put("country",customers_sorted_by_country);
-                    customersortedbycategories.put("customerid",customers_sorted_by_id);
+
+            //Übergeordnete HashMap (quasi Hauptmenü um sachen bezüglich Customer zu finden)
+            customersortedbycategories.put("customerid", customers_sorted_by_id);
+            customersortedbycategories.put("first_name_letter", customers_sorted_by_firstname_letter);
+            customersortedbycategories.put("last_name_letter", customers_sorted_by_lastname_letter);
+            customersortedbycategories.put("first_name", customers_sorted_by_firstname);
+            customersortedbycategories.put("last_name", customers_sorted_by_lastname);
+            customersortedbycategories.put("company", customers_sorted_by_company);
+            customersortedbycategories.put("address", customers_sorted_by_address);
+            customersortedbycategories.put("postalcode",customers_sorted_by_postalcode);
+            customersortedbycategories.put("city", customers_sorted_by_city);
+            customersortedbycategories.put("country", customers_sorted_by_country);
+            customersortedbycategories.put("state", customers_sorted_by_state);
+            customersortedbycategories.put("phone", customers_sorted_by_phone);
+            customersortedbycategories.put("fax", customers_sorted_by_fax);
+            customersortedbycategories.put("supportrepid", customers_sorted_by_supportrepid);
+            customersortedbycategories.put("bill", customers_sorted_by_bill);
+
+
 
         }
 
+        Scanner scanner = new Scanner(System.in);
 
-        // Übergeordnete HashMap anlegen -> HashMap<(Kategorien nach denen sortiert werden soll als string), HashMap<Unterkategorie, ArrayList<Customers> (bspw customer sorted by country oder by last name.)
-        // erst alle unterkategorien fertigstellen oder schonmal übergeordnete HashMap ausprobieren? wir erfahren es nach der Raucherpause DÖDÖDÖDÖDÖM!!!!!!
+        System.out.println("Welcome to the main menu\nWhat do you want to do??\n1-Search\n2-blabla\n3-blablabla\nInput: ");
+        int auswahlmenu = scanner.nextInt();
+        if(auswahlmenu == 1){
+            System.out.println("What do you want to search?\n1-Search in the whole Database\n2-Search in a certain column\nInput: ");
+            int auswahlsuche = scanner.nextInt();
+            if(auswahlsuche == 1){
+                return;
+            }
+            if(auswahlsuche == 2){
+                System.out.println("What column do you want to search in?\n1-Customer ID\n2-first name\n3-last name\n4-company\n5-address\n6-postal code" +
+                        "7-city\n8-country\n9-state\n10-phone\n11-fax\n12-support rep id\n13-bill");
+                int columnsuche = scanner.nextInt();
+                if(columnsuche == 1){
+                    System.out.println("Please insert what you want to search\ninsert \"all\" for every entry: ");
+                    String customeridinput = scanner.next();
+                    if (!Objects.equals(customeridinput, "all")) {
+                        System.out.println(Service.outputofcustomerwithsamestartingletters(customeridinput, customersortedbycategories.get("customerid")));
+                    }
+                    if(Objects.equals(customeridinput, "all")){
+                        System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("customerid"),""));
+                    }
+                }
+                if(columnsuche == 2){
+                    System.out.println("Please insert what you want to search\ninsert \"all\" for every entry: ");
+                    String firstnameinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("first_name"),firstnameinput));
+                }
+                if(columnsuche == 3){
+                    System.out.println("Please insert what you want to search");
+                    String lastnameinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("last_name"),lastnameinput));
+                }
+                if(columnsuche == 4){
+                    System.out.println("Please insert what you want to search");
+                    String companyinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("company"),companyinput));
+                }
+                if(columnsuche == 5){
+                    System.out.println("Please insert what you want to search");
+                    String addressinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("address"),addressinput));
+                }
+                if(columnsuche == 6){
+                    System.out.println("Please insert what you want to search");
+                    String postalcodeinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("postalcode"),postalcodeinput));
+                }
+                if(columnsuche == 7){
+                    System.out.println("Please insert what you want to search");
+                    String cityinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("city"),cityinput));
+                }
+                if(columnsuche == 8){
+                    System.out.println("Please insert what you want to search");
+                    String countryinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("country"),countryinput));
+                }
+                if(columnsuche == 9){
+                    System.out.println("Please insert what you want to search");
+                    String stateinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("state"),stateinput));
+                }
+                if(columnsuche == 10){
+                    System.out.println("Please insert what you want to search");
+                    String phoneinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("phone"),phoneinput));
+                }
+                if(columnsuche == 11){
+                    System.out.println("Please insert what you want to search");
+                    String faxinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("fax"),faxinput));
+                }
+                if(columnsuche == 12){
+                    System.out.println("Please insert what you want to search");
+                    String supportrepidinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("supportrepid"),supportrepidinput));
+                }
+                if(columnsuche == 13){
+                    System.out.println("Please insert what you want to search");
+                    String billinput = scanner.next();
+                    System.out.println(Service.valuesearchbycontainingsubstring(customersortedbycategories.get("bill"),billinput));
+                }
 
+            }
+        }
+        if (auswahlmenu == 2){
+            return;
+        }
 
-        System.out.println(customer_bill_by_id.get("38")+"$");
-
-
-        ArrayList<Customers> xyz = customers_sorted_by_country.get("Germany"); //wählt erst Key ("Germany")
-        System.out.println("\n" + xyz.get(2).getInfoShort()); //Dann Value (1) plus Getter(funktion)
-
-
-        Customers x = customers_sorted_by_country.get("Germany").get(2);//Wählt erst Key UND Value.
-        System.out.println("\n" + x.getInfoShort()); // danach nur noch Getter(funktion).
-
-        System.out.println(customers_sorted_by_country.get("Germany").get(2).getInfoShort()); //ohne Variable in einer Line
-
-        //Alle Customer eines Landes
-        Service.customersbycountry(customers_sorted_by_country,"France");
-
-
-        //Alle Customer sortiert nach Anfangsbuchstaben der Nachnamen
-        Service.outputofcustomerwithsamestartingletters("H",customers_sorted_by_lastname);
-
-
-
-        Customers oe = customers_sorted_by_id.get("5").get(0);
-        System.out.println(oe.getInfoShort());
+//        System.out.println(customersortedbycategories.get("first_name"));
+//
+//        //customersortedbycategories get first_name ist basically die referenz durch die HashMap auf die customers sorted by firstname Hashmap
+//        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+//        Service.outputofcustomerwithsamestartingletters("H", customersortedbycategories.get("first_name_letter"));
+//        Service.outputofcustomerwithsamestartingletters("H", customersortedbycategories.get("last_name_letter"));
+//        System.out.println("XYXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+//
+//        System.out.println(customers_sorted_by_firstname_letter.get("A").get(0).getInfoShort());
+//
+//
+//        Service.valuesearchbycontainingsubstring(customersortedbycategories.get("first_name"), "la");
+//        Service.valuesearchbycontainingsubstring(customersortedbycategories.get("last_name"), "H");
+//
 
     }
-
-
 
 
     static HashMap<String, Double> customer_bill_by_id = new HashMap<>();
 
     // übergeordnete HashMap um Kategorien auszuwählen
-    static HashMap<String , HashMap<String, ArrayList<Customers>>> customersortedbycategories = new HashMap<>();
+    static HashMap<String, HashMap<String, ArrayList<Customers>>> customersortedbycategories = new HashMap<>();
 
 
     //static HashMap<String, ArrayList<String>> customers_sorted_by_country = new HashMap<>();
@@ -129,8 +219,20 @@ public class Main
     //eher so mit kompletten Objekten als Value(bzw ArrayListen mit Objekten drin)
     static HashMap<String, ArrayList<Customers>> customers_sorted_by_id = new HashMap<>();
     static HashMap<String, ArrayList<Customers>> customers_sorted_by_country = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_firstname_letter = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_lastname_letter = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_firstname = new HashMap<>();
     static HashMap<String, ArrayList<Customers>> customers_sorted_by_lastname = new HashMap<>();
-
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_state = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_address = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_city = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_company = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_phone = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_postalcode = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_fax = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_email = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_supportrepid = new HashMap<>();
+    static HashMap<String, ArrayList<Customers>> customers_sorted_by_bill = new HashMap<>();
 
 
 }
--- src/Service.java
+++ src/Service.java
@@ -7,7 +7,7 @@ public class Service {
     //Erstellen einer HashMap und Rechnung einfügen insofern es für die customerid noch keine gibt
     // else -> wenn es eine gibt die Values in die ArrayList der HashMap einfügen
 
-    public static HashMap<String, ArrayList<Customers>> createkeyandfillvaluesStringArray(String keyvalue, HashMap<String, ArrayList<Customers>> hashmap,Customers customerinfo) {
+    public static HashMap<String, ArrayList<Customers>> createkeyandfillvaluesMultiValue(String keyvalue, HashMap<String, ArrayList<Customers>> hashmap,Customers customerinfo) {
         if (!hashmap.containsKey(keyvalue)) {
             hashmap.put(keyvalue, new ArrayList<>());
 
@@ -21,10 +21,24 @@ public class Service {
         return hashmap;
     }
 
-    public static HashMap<String, ArrayList<Customers>> createkeyandfillvaluesStringArrayDirectAdd(String customerid,HashMap<String, ArrayList<Customers>> list,Customers customerinfo){
-        if(!list.containsKey(customerid)){
-            list.put(customerid, new ArrayList<>());
-            list.get(customerid).add(customerinfo);
+    public static ArrayList<Customers> valuesearchbycontainingsubstring(HashMap<String, ArrayList<Customers>> customerlist,String containedsubstring) {
+        ArrayList<Customers> sortedbyinput = null;
+        for (String xxx : customerlist.keySet()) {
+            sortedbyinput = new ArrayList<>();
+            if (xxx.contains(containedsubstring)) {
+                sortedbyinput = customerlist.get(xxx);
+                for (int i = 0; i < sortedbyinput.size(); i++) {
+                    System.out.println(sortedbyinput.get(i).getInfoShort());
+                }
+            }
+        }
+        return sortedbyinput;
+    }
+
+    public static HashMap<String, ArrayList<Customers>> createkeyandfillvaluesSingleValue(String key,HashMap<String, ArrayList<Customers>> list,Customers customerinfo){
+        if(!list.containsKey(key)){
+            list.put(key, new ArrayList<>());
+            list.get(key).add(customerinfo);
         }
         return list;
     }
@@ -42,34 +56,28 @@ public class Service {
         return customerlist; //jib russ.
     }
 
-    //
-    public static ArrayList<Customers> customersbycountry(HashMap<String, ArrayList<Customers>> customerlist,String thecountryx)
-    {
-        ArrayList<Customers> bla = customerlist.get(thecountryx); //
-        for (int i = 0; i < customerlist.get(thecountryx).toArray().length; i++)
-            {
-                System.out.println(bla.get(i).getInfoShort());
-            }
-        return bla;
-    }
+
+
     //Erzeuge neuen Key für Anfangsbuchstabe des Attributes wenns keinen gibt, sonst pack jede Value mit gleichem Anfangsbuchstaben an den Key.
     // weiß nich obs überhaupt smart is den kram in ner funktion zu haben weil sortieren nach anfangsbuchstabe klingt jetz nich so krass nach
     // reusability... naja ma gucken.
-    public static HashMap<String, ArrayList<Customers>> sortingbylastnamestartingletters(String lastname,HashMap<String, ArrayList<Customers>>outputlist,Customers customerinfo) {
-        String startingletter = (lastname.substring(0, 1));
-        if (!outputlist.containsKey(startingletter)) {
-            outputlist.put(startingletter, new ArrayList<>());
-        } else {
-            outputlist.get(startingletter).add(customerinfo);
-        }
-        return outputlist;
+    public static HashMap<String, ArrayList<Customers>> sortedbystartingletters(String startingletterword, HashMap<String, ArrayList<Customers>> outputlist, Customers customerinfo) {
+            String startingletter = (startingletterword.substring(0, 1));
+            if (!outputlist.containsKey(startingletter)) {
+                outputlist.put(startingletter, new ArrayList<>());
+            } else {
+                outputlist.get(startingletter).add(customerinfo);
+            }
+            return outputlist;
     }
+
+
+
 
 
     //gib mir die ganzen Infos der Values des eingegebenen Keys
     public static ArrayList<Customers> outputofcustomerwithsamestartingletters(String lastnameletter,HashMap<String, ArrayList<Customers>> list)
     {
-    System.out.println(list.keySet());
         for(int i = 0; i < list.get(lastnameletter).toArray().length;i++) {
             ArrayList<Customers> listbyletters = list.get(lastnameletter);
             listbyletters.get(i);
@@ -77,4 +85,9 @@ public class Service {
         }
         return null;
     }
+
+
+
 }
+
+
